8b0ca8f3c1b729455b522c8bf2f80a74fbd23fac
Rudolf Cvacho <rudolf.cvacho@erni.sk>
2018-08-15 12:25:49 +0200

    LightInject - Register pattern - RegisterOrdered pattern

---
+++ src/AgentMulder.Containers.LightInject/LightInjectContainerInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Reflection;
+using AgentMulder.ReSharper.Domain.Containers;
+using AgentMulder.ReSharper.Domain.Elements.Modules;
+using AgentMulder.ReSharper.Domain.Elements.Modules.Impl;
+
+namespace AgentMulder.Containers.LightInject
+{
+    [Export(typeof(IContainerInfo))]
+    public class LightInjectContainerInfo : ContainerInfoBase
+    {
+        public override string ContainerDisplayName => "LightInject";
+
+        public override IEnumerable<string> ContainerQualifiedNames
+        {
+            get { yield return "LightInject"; }
+        }
+
+        public LightInjectContainerInfo()
+        {
+            ModuleExtractor.AddExtractor(new CallingAssemblyExtractor("LightInject.Module", "ThisAssembly"));
+        }
+
+        protected override ComposablePartCatalog GetComponentCatalog()
+        {
+            return new AssemblyCatalog(Assembly.GetExecutingAssembly());
+        }
+    }
+}
