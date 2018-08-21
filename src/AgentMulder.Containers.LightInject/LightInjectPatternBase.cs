b4abf270c809317be64fcf93cb12a1a7c5f3574a
Heliová Natália (ERNI) <hena@erni.sk>
2018-08-21 14:50:39 +0200

    Register pattern enhancements with lambda expression

---
+++ src/AgentMulder.Containers.LightInject/LightInjectPatternBase.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AgentMulder.ReSharper.Domain.Patterns;
+using JetBrains.ReSharper.Feature.Services.StructuralSearch;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace AgentMulder.Containers.LightInject
+{
+    public abstract class LightInjectPatternBase : RegistrationPatternBase
+    {
+        protected LightInjectPatternBase(IStructuralSearchPattern pattern)
+        : base(pattern)
+        {
+        }
+
+        protected IEnumerable<IDeclaredType> GetTypesFromLambda(ILambdaExpression lambdaExpression)
+        {
+            // match lambda expressions
+            if (lambdaExpression != null)
+            {
+                IDeclaredType declaredType = null;
+                if (lambdaExpression.BodyBlock != null)
+                {
+                    // lambda with statement body
+                    var returnTypes =
+                        lambdaExpression.BodyBlock.Descendants<IReturnStatement>()
+                            .ToEnumerable()
+                            .Select(_ => _?.Value?.GetExpressionType() as IDeclaredType)
+                            .Where(_ => _ != null && _.Classify == TypeClassification.REFERENCE_TYPE);
+
+                    foreach (var returnType in returnTypes)
+                    {
+                        yield return returnType;
+                    }
+                }
+                else if (lambdaExpression.BodyExpression != null)
+                {
+                    // lambda with expression body
+                    declaredType = lambdaExpression.BodyExpression.GetExpressionType() as IDeclaredType;
+                }
+
+                if (declaredType != null && declaredType.Classify == TypeClassification.REFERENCE_TYPE)
+                {
+                    yield return declaredType;
+                }
+            }
+        }
+    }
+}
--- src/AgentMulder.Containers.LightInject/Patterns/Register.cs
+++ src/AgentMulder.Containers.LightInject/Patterns/Register.cs
@@ -8,13 +8,12 @@ using JetBrains.ReSharper.Feature.Services.CSharp.StructuralSearch.Placeholders;
 using JetBrains.ReSharper.Feature.Services.StructuralSearch;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
-using JetBrains.ReSharper.Psi.Resolve;
-using JetBrains.ReSharper.Psi.Util;
+using JetBrains.ReSharper.Psi.Tree;
 
 namespace AgentMulder.Containers.LightInject.Patterns
 {
     [Export("ComponentRegistration", typeof(IRegistrationPattern))]
-    public class Register : RegisterWithService
+    public class Register : LightInjectPatternBase
     {
         private static readonly IStructuralSearchPattern pattern =
             new CSharpStructuralSearchPattern("$container$.Register($arguments$)",
@@ -26,50 +25,78 @@ namespace AgentMulder.Containers.LightInject.Patterns
         {
         }
 
-        protected override IEnumerable<IComponentRegistration> FromGenericArguments(IInvocationExpression invocationExpression)
+        public override IEnumerable<IComponentRegistration> GetComponentRegistrations(ITreeNode registrationRootElement)
         {
-            if (invocationExpression.TypeArguments.Count == 1)
+            IStructuralMatchResult match = Match(registrationRootElement);
+
+            if (match.Matched)
             {
-                if (invocationExpression.Arguments.Any())
-                {
-                    var service = invocationExpression.TypeArguments.First() as IDeclaredType;
-
-                    foreach (var componentRegistration in GetRegistrationsFromLambda(invocationExpression, service))
-                    {
-                        yield return componentRegistration;
-                    }
-                }
-
-                if (invocationExpression.TypeArguments.First() is IDeclaredType implementation)
-                {
-                    ITypeElement typeElement = implementation.GetTypeElement();
-
-                    if (typeElement != null)
-                    {
-                       yield return new ComponentRegistration(invocationExpression, typeElement);
-                    }
-                }
-            }
-            else
-            {
-                var first = invocationExpression.TypeArguments.First() as IDeclaredType;
-                var last = invocationExpression.TypeArguments.Last() as IDeclaredType;
-
-                if (first == null || last == null)
+                var invocationExpression = match.MatchedElement as IInvocationExpression;
+                if (invocationExpression == null)
                 {
                     yield break;
                 }
 
-                if (first.IsClassType() || first.IsInterfaceType())
+                if (invocationExpression.TypeArguments.Any())
                 {
-                    var service = first.GetTypeElement();
-                    var @class = last.GetTypeElement();
+                    foreach (var registration in FromGenericArguments(invocationExpression))
+                    {
+                        yield return registration;
+                    }
+                }
+                else if (invocationExpression.Arguments.Any())
+                {
+                    if (invocationExpression.Arguments.First().Value is ILambdaExpression lambdaExpression)
+                    {
+                        var implementationTypes = GetTypesFromLambda(lambdaExpression);
+                        foreach (var implementationType in implementationTypes)
+                        {
+                            var type = implementationType.GetTypeElement();
 
-                    yield return new ComponentRegistration(invocationExpression, service, @class);
+                            if (type != null)
+                            {
+                                yield return new ComponentRegistration(invocationExpression,
+                                    implementationType.GetTypeElement());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        foreach (var registration in FromArguments(invocationExpression))
+                        {
+                            yield return registration;
+                        }
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<IComponentRegistration> FromGenericArguments(IInvocationExpression invocationExpression)
+        {
+            if (!invocationExpression.Arguments.Any())
+            {
+                foreach (var componentRegistration in GetRegistrationsFromTypeArguments(invocationExpression))
+                {
+                    yield return componentRegistration;
+                }
+            }
+            else
+            {
+                if (invocationExpression.Arguments.First().Value is ILambdaExpression lambdaExpression)
+                {
+                    var implementationTypes = GetTypesFromLambda(lambdaExpression);
+
+                    var serviceType = invocationExpression.TypeArguments.Last() as IDeclaredType;
+
+                    if (serviceType != null)
+                    {
+                        yield return new TypesBasedOnRegistration(implementationTypes.Select(_ => _.GetTypeElement()),
+                            new ServiceRegistration(invocationExpression, serviceType.GetTypeElement()));
+                    }
                 }
                 else
                 {
-                    foreach (var componentRegistration in GetRegistrationsFromLambda(invocationExpression, last))
+                    foreach (var componentRegistration in GetRegistrationsFromTypeArguments(invocationExpression))
                     {
                         yield return componentRegistration;
                     }
@@ -77,29 +104,48 @@ namespace AgentMulder.Containers.LightInject.Patterns
             }
         }
 
-        private IEnumerable<IComponentRegistration> GetRegistrationsFromLambda(IInvocationExpression invocationExpression, IDeclaredType serviceType)
+        private IEnumerable<IComponentRegistration> FromArguments(IInvocationExpression invocationExpression)
         {
-            if (!invocationExpression.ArgumentList.Arguments.Any())
+            List<ITypeofExpression> arguments = invocationExpression.ArgumentList.Arguments
+                .Where(argument =>
+                {
+                    var declaredType = argument.Value.Type() as IDeclaredType;
+                    return declaredType != null && declaredType.IsType();
+                }).Select(argument => argument.Value as ITypeofExpression)
+                .ToList();
+
+            var first = arguments.First().ArgumentType as IDeclaredType;
+            var last = arguments.Last().ArgumentType as IDeclaredType;
+
+            return CreateRegistration(invocationExpression, first, last);
+        }
+
+        private IEnumerable<IComponentRegistration> GetRegistrationsFromTypeArguments(IInvocationExpression invocationExpression)
+        {
+            var first = invocationExpression.TypeArguments.First() as IDeclaredType;
+            var last = invocationExpression.TypeArguments.Last() as IDeclaredType;
+
+            foreach (var registration in CreateRegistration(invocationExpression, first, last))
+            {
+                yield return registration;
+            }
+        }
+
+        private IEnumerable<IComponentRegistration> CreateRegistration(IInvocationExpression invocationExpression, IDeclaredType first, IDeclaredType last)
+        {
+            if (first == null || last == null)
             {
                 yield break;
             }
 
-            if (invocationExpression.ArgumentList.Arguments.First().Value is ILambdaExpression lambdaExpression)
+            ITypeElement fromType = first.GetTypeElement();
+            ITypeElement toType = last.GetTypeElement();
+
+            if (fromType != null && toType != null)
             {
-                var objectCreationExpression = lambdaExpression.BodyExpression as IObjectCreationExpression;
-
-                if (objectCreationExpression?.TypeReference != null)
-                {
-                    IResolveResult resolveResult = objectCreationExpression.TypeReference.Resolve().Result;
-
-                    var service = serviceType.GetTypeElement();
-                    var @class = resolveResult.DeclaredElement as IClass;
-
-                    if (@class != null)
-                    {
-                        yield return new ComponentRegistration(invocationExpression, service, @class);
-                    }
-                }
+                yield return fromType.Equals(toType)
+                    ? new ComponentRegistration(invocationExpression, fromType)
+                    : new ComponentRegistration(invocationExpression, fromType, toType);
             }
         }
     }
