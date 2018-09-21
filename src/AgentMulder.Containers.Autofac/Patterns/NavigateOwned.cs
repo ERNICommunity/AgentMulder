5b4a215f596c959c27d5952b69d177301be01439
Rudolf Cvacho <rudolf.cvacho@erni.sk>
2018-09-21 14:05:59 +0200

    Fix circular dependency

--- src/AgentMulder.Containers.Autofac/Patterns/NavigateOwned.cs
+++ src/AgentMulder.Containers.Autofac/Patterns/NavigateOwned.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.ComponentModel.Composition;
-using System.ComponentModel.Composition.Hosting;
+﻿using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Primitives;
-using System.Linq;
-using System.Reflection;
 using AgentMulder.ReSharper.Domain.Patterns;
-using Autofac.Features.OwnedInstances;
-using JetBrains.Metadata.Reader.API;
-using JetBrains.ReSharper.Psi;
-using JetBrains.ReSharper.Psi.CSharp.Impl;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
-using JetBrains.ReSharper.Psi.Resolve;
-using JetBrains.ReSharper.Psi.Util;
-using JetBrains.ReSharper.Psi.VB.Types;
 
 namespace AgentMulder.Containers.Autofac.Patterns
 {
